2b3681698cbf5c87424a46716c13ba6cfa15ef19
jamescheng16 <jamescheng16@gmail.com>
2014-07-13 21:26:40 +0800

    create

---
+++ Xianf/Xianf/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Xianf
+//
+//  Created by james on 13/07/14.
+//  Copyright (c) 2014 Xiaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Xianf/Xianf/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Xianf
+//
+//  Created by james on 13/07/14.
+//  Copyright (c) 2014 Xiaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
